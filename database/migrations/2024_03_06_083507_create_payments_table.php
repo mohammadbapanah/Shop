696dd0c2aca6e1fc7a4baec688a777d2c3e36420
mohammadbapanah <mohammadbpn1379@gmail.com>
2024-03-06 12:17:51 +0330

    payments table create

---
+++ database/migrations/2024_03_06_083507_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 20, 3);
+            $table->foreignId('user_id')->constrained('users');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('type')->default(0)->comment('0 => online , 1 => offline , 2 => cash');
+            $table->morphs('paymentable');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
