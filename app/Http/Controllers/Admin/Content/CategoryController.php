b975012d4f587d14248f546dd147809faf7a3c82
mohammadbapanah <mohammadbpn1379@gmail.com>
2024-03-17 10:25:02 +0330

    image upload for edit method

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -84,14 +84,32 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
+     * @param PostCategoryRequest $request
      * @param int $id
-     * @return Response
+     * @param ImageService $imageService
+     * @return RedirectResponse
      */
-    public function update(PostCategoryRequest $request, $id)
+    public function update(PostCategoryRequest $request, $id, ImageService $imageService)
     {
         $postCategory = PostCategory::query()->find($id);
         $inputs = $request->all();
+        if ($request->hasFile('image')) {
+            if (!empty($postCategory->image)) {
+                $imageService->deleteDirectoryAndFiles($postCategory->image['directory']);
+            }
+            $imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'post-category');
+            $result = $imageService->createIndexAndSave($request->file('image'));
+            if ($result === false) {
+                return redirect()->route('admin.content.category.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد');
+            }
+            $inputs['image'] = $result;
+        } else {
+            if (isset($inputs['currentImage']) && !empty($postCategory->image)) {
+                $image = $postCategory->image;
+                $image['currentImage'] = $inputs['currentImage'];
+                $inputs['image'] = $image;
+            }
+        }
         $postCategory->update($inputs);
         return redirect()->route('admin.content.category.index')->with('swal-success', 'عملیات ویرایش دسته بندی پست با موفقیت انجام شد');;
     }
@@ -100,7 +118,7 @@ class CategoryController extends Controller
      * Remove the specified resource from storage.
      *
      * @param int $id
-     * @return Response
+     * @return RedirectResponse
      */
     public function destroy($id)
     {
--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -7,9 +7,9 @@
 @section('content')
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
-            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
-            <li class="breadcrumb-item font-size-12"> <a href="#">ویرایش دسته بندی</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#">خانه</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12"><a href="#">ویرایش دسته بندی</a></li>
             <li class="breadcrumb-item font-size-12 active" aria-current="page"> ویرایش دسته بندی</li>
         </ol>
     </nav>
@@ -30,7 +30,7 @@
 
                 <section>
                     <form id="form" action="{{ route('admin.content.category.update', $postCategory) }}" method="post"
-                        enctype="multipart/form-data">
+                          enctype="multipart/form-data">
                         @csrf
                         @method('put')
                         <section class="row">
@@ -39,21 +39,13 @@
                                 <div class="form-group">
                                     <label for="">نام دسته</label>
                                     <input type="text" value="{{ old('name', $postCategory->name) }}"
-                                        class="form-control form-control-sm" name="name">
+                                           class="form-control form-control-sm" name="name">
                                 </div>
                                 @error('name')
-                                    <span class="text-danger">{{ $message }}</span>
+                                <span class="text-danger">{{ $message }}</span>
                                 @enderror
                             </section>
-                            <section class="col-12 col-md-6">
-                                <div class="form-group">
-                                    <label for="">عکس سته بندی</label>
-                                    <input type="file" class="form-control form-control-sm" name="image">
-                                </div>
-                                @error('image')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </section>
+
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
                                     <label for="">وضعیت</label>
@@ -66,26 +58,66 @@
                                     </select>
                                 </div>
                                 @error('status')
-                                    <span class="text-danger">{{ $message }}</span>
+                                <span class="text-danger">{{ $message }}</span>
                                 @enderror
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
                                     <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
-                                        value="{{ old('tags', $postCategory->tags) }}">
+                                           value="{{ old('tags', $postCategory->tags) }}">
                                     <select class="select2 form-control form-control-sm" id="select_tags" multiple>
 
                                     </select>
                                 </div>
                                 @error('tags')
-                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
                                         <strong>
                                             {{ $message }}
                                         </strong>
                                     </span>
                                 @enderror
                             </section>
+
+
+                            <section class="col-12 col-md-6 my-2">
+                                <div class="form-group">
+                                    <label for="image">تصویر</label>
+                                    <input type="file" class="form-control form-control-sm" name="image" id="image">
+                                </div>
+                                @error('image')
+                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
+
+                                <section class="row">
+                                    @php
+                                        $number = 1;
+                                    @endphp
+                                    @foreach ($postCategory->image['indexArray'] as $key => $value )
+                                        <section class="col-md-{{ 6 / $number }}">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" name="currentImage"
+                                                       value="{{ $key }}" id="{{ $number }}"
+                                                       @if($postCategory->image['currentImage'] == $key) checked @endif>
+                                                <label for="{{ $number }}" class="form-check-label mx-2">
+                                                    <img src="{{ asset($value) }}" class="w-100" alt="">
+                                                </label>
+                                            </div>
+                                        </section>
+                                        @php
+                                            $number++;
+                                        @endphp
+                                    @endforeach
+
+                                </section>
+
+                            </section>
+
+
                             <section class="col-12">
                                 <div class="form-group">
                                     <label for="">توضیحات</label>
@@ -94,7 +126,7 @@
                                     </textarea>
                                 </div>
                                 @error('image')
-                                    <span class="text-danger">{{ $message }}</span>
+                                <span class="text-danger">{{ $message }}</span>
                                 @enderror
                             </section>
                             <section class="col-12">
